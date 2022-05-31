db5b8613ea2078097e3454a1b401ef547be78baf
WaylayOvercast <timelyovercast@gmail.com>
2022-05-31 17:20:26 -0500

    new react-dnd dependency and removal of unused random-interval dependency

--- client/src/Components/online/singleplayer/Singleplayer.js
+++ client/src/Components/online/singleplayer/Singleplayer.js
@@ -2,6 +2,8 @@ import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { FXhandler } from "../../howler/HowlerHandler";
 import { clickEffects } from "../../../audio/audioHandler";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 
 const init_singlePlayerGame = (socket) => {
@@ -61,55 +63,57 @@ export default function Singleplayer () {
     }
 
     return (
-        <section className='game-window'>
-            <div className='game-window-nav'>
-                <button className="game-nav-btn" 
-                    onClick={() => handleQuit()}
-                    onMouseEnter={() => FXhandler(clickEffects[2], .3)}>
-                        &#10094;
-                </button>
-                <button className="game-play-btn"
-                    onClick={() => handlePlay()}
-                    onMouseEnter={() => FXhandler(clickEffects[2], .3)}>
-                        Play
-                </button>
-                <button className="game-fire-btn"
-                    onClick={() => handleFire()}
-                    onMouseEnter={() => FXhandler(clickEffects[2], .3)}>
-                        Fire
-                </button>
-            </div>
-            <section className='enemy-board'>
-                {
-                    renderGrid().map((elm, indx) => {
-                      return <div key={`e-${indx}`} id={`tile e-${indx}`} >{`${indx}`}</div>
-                    })
-                }
-            </section>
-            <section className='self-board'>
-                {
-                    renderGrid().map((elm, indx) => {
-                      return <div key={`p-${indx}`} id={`tile p-${indx}`} >{`${indx}`}</div>
-                    })
-                }
-            </section>
-            <div className='game-commands-window'>
-                <div className="ship-carrier">
+        <DndProvider backend={HTML5Backend}>
+            <section className='game-window'>
+                <div className='game-window-nav'>
+                    <button className="game-nav-btn" 
+                        onClick={() => handleQuit()}
+                        onMouseEnter={() => FXhandler(clickEffects[2], .3)}>
+                            &#10094;
+                    </button>
+                    <button className="game-play-btn"
+                        onClick={() => handlePlay()}
+                        onMouseEnter={() => FXhandler(clickEffects[2], .3)}>
+                            Play
+                    </button>
+                    <button className="game-fire-btn"
+                        onClick={() => handleFire()}
+                        onMouseEnter={() => FXhandler(clickEffects[2], .3)}>
+                            Fire
+                    </button>
+                </div>
+                <section className='enemy-board'>
+                    {
+                        renderGrid().map((elm, indx) => {
+                        return <div key={`e-${indx}`} id={`tile e-${indx}`} >{`${indx}`}</div>
+                        })
+                    }
+                </section>
+                <section className='self-board'>
+                    {
+                        renderGrid().map((elm, indx) => {
+                        return <div key={`p-${indx}`} id={`tile p-${indx}`} >{`${indx}`}</div>
+                        })
+                    }
+                </section>
+                <div className='game-commands-window'>
+                    <div className="ship carrier">
 
-                </div>
-                <div className="ship-battleShip">
+                    </div>
+                    <div className="ship battleShip">
 
-                </div>
-                <div className="ship-destroyer">
+                    </div>
+                    <div className="ship destroyer">
 
-                </div>
-                <div className="ship-submarine">
+                    </div>
+                    <div className="ship submarine">
 
+                    </div>
+                    <div className="ship ptBoat">
+
+                    </div>
                 </div>
-                <div className="ship-ptBoat">
-                    
-                </div>
-            </div>
-        </section> 
+            </section> 
+        </DndProvider>
     )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom';
-import {RecoilRoot} from 'recoil'
+import {RecoilRoot} from 'recoil';
 
 ReactDOM.render(
   <RecoilRoot>
